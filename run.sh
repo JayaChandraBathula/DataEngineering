7bce62ee4ad62d7d1c82840cb4fe420bb665a6ab
JayaChandra Battula <39637291+JayaChandraBathula@users.noreply.github.com>
2018-07-15 12:20:04 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 #
 #python ./src/pharmacy_counting.py ./input/itcont.txt ./output/top_cost_drug.txt
 
-javac ./src/PharmacyValidation.java ./input/itcont.txt ./output/top_cost_drug.txt
+java ./src/PharmacyValidation.java ./input/itcont.txt ./output/top_cost_drug.txt
